a2454f2f34e4f17c75168f0472cda6b150f1452f
Seethanveshi <20A91A0201@aec.edu.in>
2025-09-06 14:36:43 +0530

    Make Pages more Responsive

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -132,7 +132,7 @@ function Cart() {
                                                 <div>{mobile.Name}</div>
                                                 <div className='Rating'>
                                                     <div style={{marginLeft:'3px'}}>{mobile.Rating}</div>
-                                                    <span style={{display:'flex' , alignItems:'center'}}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="1" style={{marginTop:'8px'}}><path fill="#FFF" d="M6.5 9.439l-3.674 2.23.94-4.26-3.21-2.883 4.254-.404L6.5.112l1.69 4.01 4.254.404-3.21 2.882.94 4.26z"/></svg></span>
+                                                    <span style={{display:'flex' , alignItems:'center'}}><svg className='starSymbol' xmlns="http://www.w3.org/2000/svg" width="20" height="1"><path fill="#FFF" d="M6.5 9.439l-3.674 2.23.94-4.26-3.21-2.883 4.254-.404L6.5.112l1.69 4.01 4.254.404-3.21 2.882.94 4.26z"/></svg></span>
                                                 </div>
                                             </div>
                                             <div className='deliveryDate'>
--- src/components/OrderPlacedPage/OrderPlacedPage.jsx
+++ src/components/OrderPlacedPage/OrderPlacedPage.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
+import '../../CSS/OrderPlacedPage.css'
 
 function OrderPlacedPage() {
   return (
-    <div style={{padding:'4.8rem 15% 0rem 15%'}} >
-      <div style={{display:'flex' , flexDirection:'column' , alignItems:'center' , justifyContent:'center' , gap:'10px'}}>
-        <div style={{width:'250px' , height:'300px'}}>
-          <img src="https://nyc.cloud.appwrite.io/v1/storage/buckets/6891ab870006c0711f88/files/689340fa00295cb33589/view?project=6891a8f2001fdab5d3e5&mode=admin" alt="OrderPlaced" />
+    <div className="orderPlacedPage">
+      <div className="content">
+        <div className="imageWrapper">
+          <img
+            src="https://nyc.cloud.appwrite.io/v1/storage/buckets/6891ab870006c0711f88/files/689340fa00295cb33589/view?project=6891a8f2001fdab5d3e5&mode=admin"
+            alt="OrderPlaced"
+          />
         </div>
         <div>
           <div>Your Order Placed Successfully</div>
         </div>
-        <div style={{marginTop:'20px'}}>
+        <div className="buttonWrapper">
           <a href="/Home">
-            <button onClick={() => window.location.href = '/Home'} style={{padding:'15px 40px' , background:'none' , borderRadius:'10px' , color:'green' , border:'1px solid green' , fontWeight:'bold'}} >Home Page</button>
+            <button
+              onClick={() => (window.location.href = '/Home')}
+              className="homeButton"
+            >
+              Home Page
+            </button>
           </a>
         </div>
       </div>
     </div>
+
   )
 }
 
