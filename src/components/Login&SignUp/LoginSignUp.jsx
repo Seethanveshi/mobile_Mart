d167c229e7bd4c19c3ef7f17bd58b6f9e246456e
Seethanveshi <20A91A0201@aec.edu.in>
2025-09-09 10:21:19 +0530

    Made Login and SignUp pages to fit in all screen sizes

--- src/components/Login&SignUp/LoginSignUp.jsx
+++ src/components/Login&SignUp/LoginSignUp.jsx
@@ -1,21 +1,56 @@
-import { useState } from 'react'
-import '../../App.css'
+import { useEffect, useState } from 'react'
+import '../../CSS/LoginandSignUp.css'
 import Login from '../Login/Login';
 import SignUp from '../SignUp/SignUp';
-import { useSelector } from 'react-redux'
+import { useDispatch , useSelector } from 'react-redux'
+import { updateActivePage } from '../../ReduxtoolKit/Slice/Slice';
 
 function LoginSignUp() {
+
+  const [page , setPage] = useState('Login');
   
   const isActive = useSelector((state) => state.activePage);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setPage(isActive);
+  } , [isActive])
+
+  const handlestateButton = () => {
+    if(page == 'Login'){
+      dispatch(updateActivePage('SignUp'));
+      setPage('SignUp')
+    }
+    else{
+      dispatch(updateActivePage('Login'));
+      setPage('Login');
+    }
+  }
 
   return (
-    <div style={{boxSizing:'border-box' , marginTop:'5rem' , height:'89vh', display:'flex' , alignItems:'center' , justifyContent:'center'}}>
+    <div className='loginandSignUppage'>
       <div className='Login_SignUp_Page' > 
           <div className={`slider ${isActive == 'Login' ? 'Active' : ''}`}></div>
           <Login />
           <SignUp />
       </div>
+
+      <div>
+        <div className='LoginAndSignUpPage' >
+            {
+              page == 'Login' ? <Login /> : <SignUp />
+            }
+            <div className='statement'>
+              {page == 'Login' ? 'newUser?' : 'Already Have an account?'}
+              <button className='stateButton'
+                onClick={() => handlestateButton()}
+              >{page == 'Login' ? 'Register' : 'Login'}</button>
+            </div>
+              
+        </div>
+      </div>
     </div>
+
   )
 }
 
